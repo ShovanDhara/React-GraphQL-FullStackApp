91f3b51e5fcaea1c582b25e4f2f7f57fc134131f
Shovan Dhara <shovan.9836@gmail.com>
2020-04-22 17:34:02 +0530

    Commit for client code changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import './App.css';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from "@apollo/react-hooks";
-import { PokemonsContainer } from './containers/pokemonContainer';
+import LinkList from './components/linklist'
+import CreateLink from './components/createlink'
+import Header from './components/header'
+import Login from './components/login'
+import './styles/App.css'
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
-  const client = new ApolloClient({
-    uri: 'https://graphql-pokemon.now.sh'
-  });
-
   return (
-    <ApolloProvider client={client}>
-      <PokemonsContainer />
-    </ApolloProvider>
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </div>
+    </div>
   );
 }
 
