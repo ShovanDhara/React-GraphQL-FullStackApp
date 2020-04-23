ed84aadcfc5a001fea0f44556a53e91fd41c09e1
Shovan Dhara <shovan.9836@gmail.com>
2020-04-23 13:20:39 +0530

    Commit for final code push

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/header'
 import Login from './components/login'
 import Search from './components/search'
 import './styles/App.css'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 function App() {
   return (
@@ -13,7 +13,9 @@ function App() {
       <Header />
       <div className="ph3 pv1 background-gray">
         <Switch>
-          <Route exact path="/" component={LinkList} />
+          <Route exact path='/' render={() => <Redirect to='/new/1' />} />
+          <Route exact path='/top' component={LinkList} />
+          <Route exact path='/new/:page' component={LinkList} />
           <Route exact path="/create" component={CreateLink} />
           <Route exact path="/login" component={Login} />
           <Route exact path='/search' component={Search} />
